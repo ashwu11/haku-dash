e33cf82a75bbc7ec44a13338e5998fa6b98363aa
Ashley Wu <ashleywu2004@gmail.com>
2024-02-13 18:01:32 -0800

    add obstacles and implement obstacle collision detection

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const woodImg = document.getElementById('woodImage')
 const floorImg = document.getElementById('floorImage')
 const nofaceImg = document.getElementById('nofaceImage')
 const playerImg = document.getElementById('chihiroImage')
+const endingImg = document.getElementById('hakuImage')
+const sootRockImg = document.getElementById('sootRockImage')
 const gravity = 0.6
 const keys = {right: false, left: false}
 const jumpSpeed = 11
-const winPosition = 2000
+const winPosition = 5000
 
 // CLASSES
 class Player {
@@ -20,7 +22,7 @@ class Player {
         this.width = 85
         this.height = 160
         this.velocity = {x: 10, y: 10}
-        this.speed = 6
+        this.speed = 10
         this.image = playerImg
         this.frame = 0;
         this.counter = 20
@@ -80,11 +82,25 @@ class Scenery {
     }
 }
 
+class Obstacle {
+    constructor({x, y, w, h, img}) {
+        this.image = img
+        this.position = {x, y}
+        this.width = w
+        this.height = h
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 // GAME
 let progress = 0
 let player = new Player()
 let platforms = []
 let sceneryObjects = []
+let obstacles = []
 
 init()
 animate()
@@ -93,23 +109,34 @@ addEventListener('keyup', handleKeyUp)
 
 // FUNCTIONS
 function init() {
+    progress = 0 
     player = new Player()
     platforms = [ 
         new Platform({x: -1, y: 500, w: 500, h: 100, img: floorImg}),
+        new Platform({x: 300, y: 425, w: 75, h: 75, img: sootRockImg}),
         new Platform({x: floorImg.width - 2, y: 500, w: 500, h: 100, img: floorImg}),
         new Platform({x: floorImg.width*2 + 200, y: 500, w: 500, h: 100, img: floorImg}),
-        new Platform({x: floorImg.width*3 + 350, y:400, w: 150, h: 50, img: woodImg}),
+        new Platform({x: floorImg.width*3 + 350, y: 400, w: 150, h: 50, img: woodImg}),
         new Platform({x: floorImg.width*3 + 600, y: 500, w: 500, h: 100, img: floorImg}),
-        new Platform({x: floorImg.width*4 + 600, y: 500, w: 500, h: 100, img: floorImg}),
-        new Platform({x: floorImg.width*5 + 500, y:400, w: 150, h: 50, img: woodImg}),
-        new Platform({x: floorImg.width*6 + 500, y: 500, w: 500, h: 100, img: floorImg})
+        new Platform({x: floorImg.width*4 + 500, y: 400, w: 150, h: 50, img: woodImg}),
+        new Platform({x: floorImg.width*4 + 700, y: 300, w: 150, h: 50, img: woodImg}),
+        new Platform({x: floorImg.width*5 + 500, y: 500, w: 500, h: 100, img: floorImg}),
+        new Platform({x: floorImg.width*6 + 500, y: 500, w: 500, h: 100, img: floorImg}),
+        new Platform({x: floorImg.width*6 + 500, y: 425, w: 75, h: 75, img: sootRockImg}),
+        new Platform({x: floorImg.width*7 + 500, y: 500, w: 500, h: 100, img: floorImg}),
+        new Platform({x: floorImg.width*8 + 400, y: 425, w: 75, h: 75, img: sootRockImg}),
+        new Platform({x: floorImg.width*8 + 900, y: 500, w: 500, h: 100, img: floorImg}),
+        new Platform({x: floorImg.width*9 + 900, y: 500, w: 500, h: 100, img: floorImg})
     ]
     sceneryObjects = [
         new Scenery({x: 0, y: 0, w: canvas.width, h: canvas.height, img: backgroundImg}),
         new Scenery({x: 700, y: 200, w: nofaceImg.width, h: nofaceImg.height, img: nofaceImg})
     ]
-
-    progress = 0    
+    obstacles = [
+        new Obstacle({x: 500, y: 345, w: nofaceImg.width, h: nofaceImg.height, img: nofaceImg}),
+        new Obstacle({x: floorImg.width*6 + 700, y: 345, w: nofaceImg.width, h: nofaceImg.height, img: nofaceImg}),
+        new Obstacle({x: winPosition + 500, y: 200, w: 225, h: 300, img: endingImg})
+    ]   
 }
 
 function animate() {
@@ -124,9 +151,14 @@ function animate() {
     platforms.forEach((platform) => {
         platform.draw()
     })
+
+    obstacles.forEach((obstacle) => {
+        obstacle.draw()
+    })
+
     player.update()
     
-
+    // player movement
     if (keys.left && player.position.x > 50) {
         player.velocity.x = -player.speed
     } else if (keys.right && player.position.x < 450) {
@@ -134,14 +166,16 @@ function animate() {
     } else {
         player.velocity.x = 0
 
-        // scroll background
+        // scroll background and obstacles
         if (keys.right) {
             progress += player.speed
             platforms.forEach((platform) => { platform.position.x -= player.speed })
+            obstacles.forEach((obstacle) => { obstacle.position.x -= player.speed })
         }
         if (keys.left && progress > 0) {
             progress -= player.speed
             platforms.forEach((platform) => { platform.position.x += player.speed })
+            obstacles.forEach((obstacle) => { obstacle.position.x += player.speed })
         }
     }
 
@@ -159,15 +193,25 @@ function animate() {
 
     console.log(progress)
     // win condition
-    if (progress > winPosition) {
+    if (progress >= winPosition) {
         console.log("Congrats! You win!")
     }
 
     // lose condition
     if (player.position.y > canvas.height) {
-        console.log("You lose...womp womp")
+        console.log("You lose... womp womp")
         init()
     }
+
+    // obstacle collision detection
+    obstacles.forEach((obstacle) => {
+        if (player.position.x + player.width/2 >= obstacle.position.x &&
+            player.position.x + player.width <= obstacle.position.x + obstacle.width &&
+            player.position.y + player.height >= obstacle.position.y) {
+            console.log("You lose... womp womp")
+            init()
+        }
+    })
 }
 
 function handleKeyDown(event) {
@@ -205,15 +249,9 @@ function handleKeyUp(event) {
         case 'a':
             keys.left = false
             break
-        
-        case 'w':
-            break
-        
+             
         case 'd':
             keys.right = false
-            break
-
-        case 's':
             break
 
     }
