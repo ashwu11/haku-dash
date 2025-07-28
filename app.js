798d6d247b4ff87c51371d01f25fd93c27165d63
Ashley Wu <ashleywu2004@gmail.com>
2025-07-28 00:48:14 -0700

    refactor and fix walking audio

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ import Player from "./classes/Player.js";
 import Scenery from "./classes/Scenery.js";
 import Obstacle from "./classes/Obstacle.js";
 
-// Audio
-const audio = document.getElementById('mainAudio');
-const walkSound = document.getElementById('walkingAudio');
-const jumpSound = document.getElementById('jumpAudio');
-const windSound = document.getElementById('windAudio');
-const thumpSound = document.getElementById('thumpAudio');
-const tadaSound = document.getElementById('tadaAudio');
+// Sounds
+const audio = {
+    main: document.getElementById('mainAudio'),
+    walk: document.getElementById('walkingAudio'),
+    jump: document.getElementById('jumpAudio'),
+    wind: document.getElementById('windAudio'),
+    thump: document.getElementById('thumpAudio'),
+    tada: document.getElementById('tadaAudio')
+};
 
 // Images
-const backgroundImg = document.getElementById('backgroundImage');
-const woodImg = document.getElementById('woodImage');
-const floorImg = document.getElementById('floorImage');
-const nofaceImg = document.getElementById('nofaceImage');
-const playerImg = document.getElementById('chihiroImage');
-const endingImg = document.getElementById('hakuImage');
-const sootRockImg = document.getElementById('sootRockImage');
+const images = {
+    background: document.getElementById('backgroundImage'),
+    wood: document.getElementById('woodImage'),
+    floor: document.getElementById('floorImage'),
+    noFace: document.getElementById('nofaceImage'),
+    player: document.getElementById('chihiroImage'),
+    ending: document.getElementById('hakuImage'),
+    soot: document.getElementById('sootRockImage')
+}
 
 // Game elements
 const gameContainer = document.getElementById('game');
@@ -29,7 +33,7 @@ const playAgainButton = document.getElementById("playAgain");
 
 // Canvas setup
 const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
@@ -38,7 +42,7 @@ const winPosition = 6600;
 const gravity = 0.88;
 const jumpSpeed = 13;
 const keys = { right: false, left: false };
-const captionSteps = [
+const captions = [
     { threshold: 0, instruction: 'Use WAD keys to move' },
     { threshold: 50, instruction: 'Turn your volume up :)', message: 'Help Chihiro find Haku!' },
     { threshold: 800, instruction: '._.' },
@@ -52,7 +56,7 @@ const captionSteps = [
 
 // Variables
 let progress = 0;
-let player = new Player(gravity, playerImg, keys);
+let player = new Player(gravity, images.player, keys);
 let platforms = [];
 let sceneryObjects = [];
 let obstacles = [];
@@ -82,56 +86,55 @@ playAgainButton.addEventListener('click', () => {
 
 // initialize a new game
 function startGame() {
-    audio.play();
-    walkSound.play();
+    audio.main.play();
     progress = 0;
-    player = new Player(gravity, playerImg, keys);
+    player = new Player(gravity, images.player, keys);
     platforms = [
-        new Platform({ x: -1, y: 500, w: 500, h: 100, img: floorImg }),
-        new Platform({ x: floorImg.width - 2, y: 500, w: 500, h: 100, img: floorImg }),
+        new Platform({ x: -1, y: 500, w: 500, h: 100, img: images.floor }),
+        new Platform({ x: images.floor.width - 2, y: 500, w: 500, h: 100, img: images.floor }),
         // death pits
-        new Platform({ x: floorImg.width * 2 + 200, y: 500, w: 500, h: 100, img: floorImg }),
-        new Platform({ x: floorImg.width * 3 + 325, y: 420, w: 150, h: 50, img: woodImg }),
-        new Platform({ x: floorImg.width * 4 + 100, y: 500, w: 500, h: 100, img: floorImg }),
+        new Platform({ x: images.floor.width * 2 + 200, y: 500, w: 500, h: 100, img: images.floor }),
+        new Platform({ x: images.floor.width * 3 + 325, y: 420, w: 150, h: 50, img: images.wood }),
+        new Platform({ x: images.floor.width * 4 + 100, y: 500, w: 500, h: 100, img: images.floor }),
         // floating wood
-        new Platform({ x: floorImg.width * 5 + 120, y: 400, w: 150, h: 50, img: woodImg }),
-        new Platform({ x: floorImg.width * 5 + 310, y: 300, w: 150, h: 50, img: woodImg }),
-        new Platform({ x: floorImg.width * 5 + 500, y: 400, w: 150, h: 50, img: woodImg }),
-        new Platform({ x: floorImg.width * 6 + 200, y: 500, w: 500, h: 100, img: floorImg }),
+        new Platform({ x: images.floor.width * 5 + 120, y: 400, w: 150, h: 50, img: images.wood }),
+        new Platform({ x: images.floor.width * 5 + 310, y: 300, w: 150, h: 50, img: images.wood }),
+        new Platform({ x: images.floor.width * 5 + 500, y: 400, w: 150, h: 50, img: images.wood }),
+        new Platform({ x: images.floor.width * 6 + 200, y: 500, w: 500, h: 100, img: images.floor }),
         // soot sprites and No Face
-        new Platform({ x: floorImg.width * 7 + 200, y: 500, w: 500, h: 100, img: floorImg }),
-        new Platform({ x: floorImg.width * 7 + 300, y: 425, w: 55, h: 75, img: sootRockImg }),
-        new Platform({ x: floorImg.width * 8 + 200, y: 500, w: 500, h: 100, img: floorImg }),
+        new Platform({ x: images.floor.width * 7 + 200, y: 500, w: 500, h: 100, img: images.floor }),
+        new Platform({ x: images.floor.width * 7 + 300, y: 425, w: 55, h: 75, img: images.soot }),
+        new Platform({ x: images.floor.width * 8 + 200, y: 500, w: 500, h: 100, img: images.floor }),
         // balance on logs
-        new Platform({ x: floorImg.width * 9 + 370, y: 500, w: 150, h: 50, img: woodImg }),
-        new Platform({ x: floorImg.width * 10 + 100, y: 500, w: 150, h: 50, img: woodImg }),
-        new Platform({ x: floorImg.width * 10 + 400, y: 500, w: 500, h: 100, img: floorImg }),
+        new Platform({ x: images.floor.width * 9 + 370, y: 500, w: 150, h: 50, img: images.wood }),
+        new Platform({ x: images.floor.width * 10 + 100, y: 500, w: 150, h: 50, img: images.wood }),
+        new Platform({ x: images.floor.width * 10 + 400, y: 500, w: 500, h: 100, img: images.floor }),
         // final puzzle
-        new Platform({ x: floorImg.width * 11 + 300, y: 500, w: 500, h: 100, img: floorImg }),
-        new Platform({ x: floorImg.width * 11 + 350, y: 425, w: 55, h: 75, img: sootRockImg }),
-        new Platform({ x: floorImg.width * 11 + 500, y: 325, w: 150, h: 50, img: woodImg }),
-        new Platform({ x: floorImg.width * 12 + 100, y: 500, w: 500, h: 100, img: floorImg }),
-        new Platform({ x: floorImg.width * 13 + 100, y: 500, w: 500, h: 100, img: floorImg }),
+        new Platform({ x: images.floor.width * 11 + 300, y: 500, w: 500, h: 100, img: images.floor }),
+        new Platform({ x: images.floor.width * 11 + 350, y: 425, w: 55, h: 75, img: images.soot }),
+        new Platform({ x: images.floor.width * 11 + 500, y: 325, w: 150, h: 50, img: images.wood }),
+        new Platform({ x: images.floor.width * 12 + 100, y: 500, w: 500, h: 100, img: images.floor }),
+        new Platform({ x: images.floor.width * 13 + 100, y: 500, w: 500, h: 100, img: images.floor }),
         // win platform
-        new Platform({ x: floorImg.width * 14 + 150, y: 430, w: 150, h: 50, img: woodImg })
+        new Platform({ x: images.floor.width * 14 + 150, y: 430, w: 150, h: 50, img: images.wood })
     ];
     sceneryObjects = [
-        new Scenery({ x: 0, y: 0, w: canvas.width, h: canvas.height, img: backgroundImg }),
-        new Scenery({ x: 700, y: 200, w: nofaceImg.width, h: nofaceImg.height, img: nofaceImg }),
-        new Scenery({ x: 820, y: 270, w: 55, h: 75, img: sootRockImg }),
+        new Scenery({ x: 0, y: 0, w: canvas.width, h: canvas.height, img: images.background }),
+        new Scenery({ x: 700, y: 200, w: images.noFace.width, h: images.noFace.height, img: images.noFace }),
+        new Scenery({ x: 820, y: 270, w: 55, h: 75, img: images.soot }),
     ];
     obstacles = [
-        new Obstacle({ x: floorImg.width * 8 - 10, y: 345, w: nofaceImg.width, h: nofaceImg.height, img: nofaceImg }),
-        new Obstacle({ x: floorImg.width * 12 + 270, y: 345, w: nofaceImg.width, h: nofaceImg.height, img: nofaceImg }),
-        new Obstacle({ x: winPosition + 700, y: 180, w: 230, h: 300, img: endingImg })
+        new Obstacle({ x: images.floor.width * 8 - 10, y: 345, w: images.noFace.width, h: images.noFace.height, img: images.noFace }),
+        new Obstacle({ x: images.floor.width * 12 + 270, y: 345, w: images.noFace.width, h: images.noFace.height, img: images.noFace }),
+        new Obstacle({ x: winPosition + 700, y: 180, w: 230, h: 300, img: images.ending })
     ];
 }
 
 // handle game animation
 function animate() {
     requestAnimationFrame(animate);
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // draw all elements
     sceneryObjects.forEach((sceneryObject) => { sceneryObject.draw() });
@@ -160,6 +163,14 @@ function animate() {
         }
     }
 
+    // walking sound 
+    const isWalking = keys.left || keys.right;
+    if (isWalking) {
+        if (audio.walk.paused) audio.walk.play();
+    } else {
+        if (!audio.walk.paused) audio.walk.pause();
+    }
+
     // platform collision detection
     platforms.forEach((platform) => {
         if (player.position.y + player.height <= platform.position.y + 10 && // top
@@ -173,7 +184,7 @@ function animate() {
     console.log(progress);
 
     // caption changes
-    let currentStep = captionSteps.findLast(step => progress >= step.threshold);
+    let currentStep = captions.findLast(step => progress >= step.threshold);
     if (currentStep) {
         if (currentStep.message) message.innerHTML = currentStep.message;
         if (currentStep.instruction) instruction.innerHTML = currentStep.instruction;
@@ -183,7 +194,7 @@ function animate() {
     if (progress > winPosition) {
         message.innerHTML = 'Yay, we found Haku!';
         console.log("You win!");
-        tadaSound.play();
+        audio.tada.play();
         showWinScreen();
     }
 
@@ -191,7 +202,7 @@ function animate() {
     if (player.position.y > canvas.height) {
         console.log("You lose... womp womp");
         message.innerHTML = 'ahhh... try again :]';
-        windSound.play();
+        audio.wind.play();
         startGame();
     }
 
@@ -202,13 +213,8 @@ function animate() {
             player.position.y + player.height - 35 >= obstacle.position.y) {
             console.log("You lose... womp womp");
             message.innerHTML = 'ouch... try again :]';
-            thumpSound.play();
+            audio.thump.play();
             startGame();
-            // document.getElementById('chihiroImage').classList.add('fadeOut')
-            // setTimeout(() => {
-            //     startGame()
-            // }, 3000)
-            // I think the image doesn't actually disappear, is being overwritten so it lags
         }
     })
 }
@@ -226,7 +232,7 @@ function handleKeyDown(event) {
             console.log('up');
             if (player.velocity.y == 0) {
                 player.velocity.y -= jumpSpeed;
-                jumpSound.play();
+                audio.jump.play();
             }
             break;
 
@@ -235,9 +241,6 @@ function handleKeyDown(event) {
             keys.right = true;
             break;
 
-        case 's':
-            console.log('down');
-            break;
     }
 }
 
@@ -281,10 +284,10 @@ function showWinScreen() {
 
 // set up audio volume 
 function setAudioVolume() {
-    audio.volume = 0.3;
-    jumpSound.volume = 0.3;
-    walkSound.volume = 0.6;
-    windSound.volume = 0.3;
-    thumpSound.volume = 0.3;
-    tadaSound.volume = 0.1;
+    audio.main.volume = 0.3;
+    audio.jump.volume = 0.3;
+    audio.walk.volume = 0.6;
+    audio.wind.volume = 0.3;
+    audio.thump.volume = 0.3;
+    audio.tada.volume = 0.1;
 }
